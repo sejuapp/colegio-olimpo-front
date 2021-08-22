5de9c940244753563fc26d03da1e88475959f29b
DESKTOP-3SRVM78\ASUS-S510UN <givandavid@gmail.com>
2021-08-21 22:46:20 -0500

    Agregado informacion conteo

--- src/app/views/profesor/asignatura-estudiante/asignatura-estudiante.component.ts
+++ src/app/views/profesor/asignatura-estudiante/asignatura-estudiante.component.ts
@@ -9,24 +9,30 @@ import { MatAccordion } from '@angular/material/expansion';
   styleUrls: ['./asignatura-estudiante.component.scss']
 })
 export class AsignaturaEstudianteComponent implements OnInit {
-  @Input() profesorId : number = 0;
+  @Input() profesorId: number = 0;
   @ViewChild(MatAccordion) miAccordion!: MatAccordion;
 
   msgs: MessageCustom[] = [];
-  loading : boolean = false;
-  lstAsignaturas : Array<any> = [];
+  loading: boolean = false;
+  lstAsignaturas: Array<any> = [];
+
+  reporte = {
+    totalAlumnos: 0,
+    totalMaterias: 0,
+    totalAsignaciones: 0
+  }
 
   displayedColumns: string[] = ['icon', 'estudianteNombre'];
 
   constructor(
-    private srvAsignatura : AsignaturaService
+    private srvAsignatura: AsignaturaService
   ) { }
 
   ngOnInit(): void {
     this.consultaInicial();
   }
 
-  ngOnChanges(changes : any) {
+  ngOnChanges(changes: any) {
     if (changes != null) {
       if (changes.profesorId != null) {
         this.consultaInicial();
@@ -34,27 +40,58 @@ export class AsignaturaEstudianteComponent implements OnInit {
     }
   }
 
-  async consultaInicial(){
+  async consultaInicial() {
     try {
       this.msgs = [];
       this.lstAsignaturas = [];
-      if(this.profesorId != 0){
+      this.contadoresCero();
+      if (this.profesorId != 0) {
         this.loading = true;
         this.lstAsignaturas = await this.srvAsignatura.findByProfesor(this.profesorId).toPromise();
-        setTimeout(()=> {this.loading = false;}, 900);
+        this.contar();
+        setTimeout(() => { this.loading = false; }, 900);
       }
 
     } catch (error) {
       this.loading = false;
-      this.msgs.push({ severity: 'e', summary: 'Error', detail: `Ha ocurrido un error al interntar realizar la consulta de asignaturas y estudiantes: ${error.message}`  });
+      this.msgs.push({ severity: 'e', summary: 'Error', detail: `Ha ocurrido un error al interntar realizar la consulta de asignaturas y estudiantes: ${error.message}` });
     }
   }
 
-  openAll(){
+  contadoresCero(){
+    this.reporte.totalAlumnos = 0;
+    this.reporte.totalMaterias = 0;
+    this.reporte.totalAsignaciones = 0;
+  }
+
+  contar() {
+
+    this.contadoresCero();
+
+    let contador = 0;
+    for (const iterator of this.lstAsignaturas) {
+      contador += iterator.lstEstudiantes.length;
+    }
+
+
+    let valor = this.lstAsignaturas.reduce((groups, item) => {
+      var val = item['nombre'];
+      groups[val] = groups[val] || null;
+      return groups;
+    }, {});
+
+    this.reporte.totalAlumnos = contador;
+    this.reporte.totalMaterias = Object.keys(valor).length;
+    this.reporte.totalAsignaciones = this.lstAsignaturas.length;
+
+    return contador;
+  }
+
+  openAll() {
     this.miAccordion.openAll();
   }
 
-  closeAll(){
+  closeAll() {
     this.miAccordion.closeAll();
   }
 }
